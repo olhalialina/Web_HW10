2895ec557a3b2126f5cbaee72439244e9a258e51
olhalialina <145963563+olhalialina@users.noreply.github.com>
2024-03-28 14:13:22 +0200

    Add files via upload

---
+++ project_quotes/users/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+
+from . import views
+
+from django.contrib.auth.views import LogoutView   #LoginView,
+
+from .forms import LoginForm
+from .views import RegisterView#, LoginView
+
+app_name = "users"
+
+urlpatterns = [
+    path('signup/', RegisterView.as_view(), name='signup'),
+
+    path('signin/', views.signin, name='signin'),
+
+    # path('signin/', LoginView.as_view(
+    #     template_name="users/signin.html",
+    #     authentication_form=LoginForm,
+    #     redirect_authenticated_user=True
+    # ), name='signin'),
+
+    # path('signin/', LoginView.as_view(), name='signin'),
+    # path('signup/', views.signup, name='signup'),
+
+    path('logout/', views.logoutuser, name='logoutuser'),
+]
